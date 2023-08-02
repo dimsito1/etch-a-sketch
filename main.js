dc6e19d473088dc5ba3f76c172d22b56f1d3a2cb
Dimislav <dimivelkov1@gmail.com>
2023-08-02 12:38:23 +0300

    format changes

--- main.js
+++ main.js
@@ -2,21 +2,18 @@ let container = document.querySelector(".container");
 let startButton = document.createElement("button");
 
 function removeStartButton() {
-    startButton.classList.add("start-button");
+  startButton.classList.add("start-button");
 
-    container.appendChild(startButton);
+  container.appendChild(startButton);
 }
 
 function fillGridElements() {
+  for (var i = 0; i < 256; i++) {
+    let gridElement = document.createElement("div");
+    gridElement.classList.add("div-element");
 
-
-    for (var i = 0; i < 256; i++) {
-        let gridElement = document.createElement("div");
-        gridElement.classList.add("div-element");
-        
-        container.appendChild(gridElement);
-    }
-
+    container.appendChild(gridElement);
+  }
 }
 
 fillGridElements();
