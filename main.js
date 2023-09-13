e17517d10522bc03a527649eed633e01d4b2ba06
Dimislav <dimivelkov1@gmail.com>
2023-09-13 18:50:20 +0300

    added starting animation

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 let container = document.querySelector(".container");
-let startButton = document.createElement("button");
+let startButton = document.querySelector(".button-19");
 
-function removeStartButton() {
-  startButton.classList.add("start-button");
-
-  container.appendChild(startButton);
-}
+const GRID_SIZE = 16;
+const DELAY_PER_ELEMENT = 8;
 
 function fillGridElements() {
-  for (var i = 0; i < 256; i++) {
+  for (let index = 0; index < GRID_SIZE * GRID_SIZE; index++) {
     let gridElement = document.createElement("div");
     gridElement.classList.add("div-element");
-
     container.appendChild(gridElement);
   }
 }
 
 fillGridElements();
+
+startButton.addEventListener('click', () => {
+  startButton.style.cursor = "default"; 
+  startButton.style.opacity = "0";
+
+  setTimeout(() => {
+    startButton.style.display = "none";
+  }, 1000); 
+
+  let gridElements = document.querySelectorAll(".div-element");
+
+  for (let index = 0; index < GRID_SIZE * GRID_SIZE; index++) {
+    setTimeout(() => {
+      gridElements[index].style.backgroundColor = "white";
+      gridElements[index].classList.add("active");
+    }, DELAY_PER_ELEMENT * index);
+  }
+});
